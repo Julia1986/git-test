6f4193bbd1dc8218f6af1b4cbf2bfdf4541af6cd
Juliya <prakhova_yuliya@ukr.net>
2020-03-20 23:13:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let arr = ['plum.png', 'apple.jpg', 'orange.png'];
+/*console.log(arr[0]);
+let answer = confirm("Are you here?");
+console.log(answer);*/
+let answer = prompt("18");
+console.log(typeof(arr));
